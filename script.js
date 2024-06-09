5fa820fe029580872dfcb45009f0f3af677f8446
Aiman Iskandar <esxkandar14@gmail.com>
2024-06-09 21:03:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ navLinks.forEach(link => {
         link.classList.add('active');
     }
 });
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if(username && password)
+        window.location.href = 'info.html';
+});
+
+const currentPath = window.location.pathname;
+
+const navLinks = document.querySelectorAll('.nav a');
+
+navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentPath.split('/').pop()) {
+        link.classList.add('active');
+    }
+});
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if(username && password)
+        window.location.href = 'info.html';
+});
+
